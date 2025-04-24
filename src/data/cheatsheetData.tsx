120df26c1f2353f4ea470ebe1d314ca2c2210677
sait <codersait@gmail.com>
2025-04-24 10:57:13 +0300

    Add example for class and object concepts in cheatsheetData to enhance understanding of OOP fundamentals

--- src/data/cheatsheetData.tsx
+++ src/data/cheatsheetData.tsx
@@ -541,6 +541,14 @@ finally:
     title: 'Classes',
     examples: [
       {
+        description: 'Class & Object Concepts',
+        code: `# Class: blueprint for creating new objects
+# Object: instance of a class
+
+# Class: Human
+# Objects: John, Mary, Jack,`,
+      },
+      {
         description: 'Creating Classes',
         code: `class Point:
     def __init__(self, x, y):
